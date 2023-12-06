e368562da474f58c26794a93142ed23b8756aae0
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-12-06 07:58:42 -0300

    Add unit tests

--- ShopCartTests/ShopCartTests.swift
+++ ShopCartTests/ShopCartTests.swift
@@ -8,29 +8,104 @@
 import XCTest
 @testable import ShopCart
 
-final class ShopCartTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class ServiceMock: ServiceProtocol {
+    
+    
+    func getProducts(url: AppConstants.Urls, completion: @escaping (Result<ProductList, Error>) -> Void) {
+        guard let jsonData = loadDataFromMock() else {
+            completion(.failure(NetworkErrors.badRequest))
+            return
         }
+        guard let list = try? JSONDecoder().decode(ProductList.self, from: jsonData) else {
+            completion(.failure(NetworkErrors.decodingError))
+            return
+        }
+        completion(.success(list))
     }
-
+    
+    func loadDataFromMock() -> Data? {
+        guard let url = Bundle.main.url(forResource: "product", withExtension: "json")
+            else {
+                print("Json file not found")
+                return nil
+            }
+        
+        let data = try? Data(contentsOf: url)
+        return data
+    }
 }
+
+final class desafioAleloTests: XCTestCase {
+
+    func test_filter_sales() {
+        let viewModel = HomeViewModel(manager: HomeManager(webservice: ServiceMock()))
+        viewModel.selectedFilter = .onSale
+        let containsRegularProducts = viewModel.filteredProducts.contains(where: { !$0.onSale })
+        XCTAssertFalse(containsRegularProducts, "Should be false to show no regular products are included, only on sale.")
+    }
+    
+    
+    func test_total_prices_calculation() {
+        let viewModel = CartViewModel.shared
+        let mockProducts = mock_cart
+        viewModel.products = mockProducts
+        let productsTotalEqualsExpected = viewModel.getTotalValue() == "R$ 810,00"
+        XCTAssertTrue(productsTotalEqualsExpected, "Should be true if the total price is correct (using mock)")
+    }
+    
+    func test_repeated_products_rule() {
+        let viewModel = CartViewModel.shared
+        let mockProduct = mock_cart[0]
+        viewModel.addProduct(item: mockProduct)
+        viewModel.addProduct(item: mockProduct)
+        XCTAssertTrue(viewModel.products.count == 1, "Should be true if repeated products are not beaing added again, which is correct")
+    }
+    
+    func test_remove_button() {
+        let viewModel = CartViewModel.shared
+        let mockProducts = mock_cart
+        viewModel.products = mockProducts
+        viewModel.removeProduct(item: mockProducts[0])
+        let viewModelContainsRemovedProduct = viewModel.products.contains(where: { $0 == mockProducts[0] })
+        XCTAssertFalse(viewModelContainsRemovedProduct, "Should be false if the product was successfully removed")
+    }
+}
+
+let mock_cart = [
+    CartItem(product: Product(name: "T-SHIRT",
+                              style: "night",
+                              codeColor: "%#$%#",
+                              colorSlug: "%$%$#%$#",
+                              color: "Blue",
+                              onSale: true,
+                              regularPrice: "R$ 100",
+                              actualPrice: "R$ 90",
+                              discountPercentage: "10%",
+                              installments: "test",
+                              image: "breat",
+                              sizes: [Size(available: true, size: "P", sku: "gf433t")]), amount: 1, size: "PP"),
+    CartItem(product: Product(name: "T-SHIRT",
+                              style: "night",
+                              codeColor: "%#$%#",
+                              colorSlug: "%$%$#%$#",
+                              color: "Blue",
+                              onSale: true,
+                              regularPrice: "R$ 100",
+                              actualPrice: "R$ 90",
+                              discountPercentage: "10%",
+                              installments: "test",
+                              image: "breat",
+                              sizes: [Size(available: true, size: "P", sku: "gf433t")]), amount: 3, size: "P"),
+    CartItem(product: Product(name: "T-SHIRT",
+                              style: "night",
+                              codeColor: "%#$%#",
+                              colorSlug: "%$%$#%$#",
+                              color: "Blue",
+                              onSale: true,
+                              regularPrice: "R$ 100",
+                              actualPrice: "R$ 90",
+                              discountPercentage: "10%",
+                              installments: "test",
+                              image: "breat",
+                              sizes: [Size(available: true, size: "P", sku: "gf433t")]), amount: 5, size: "U")
+]
