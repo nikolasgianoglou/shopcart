30d9a7caee21c689572bc439bf8d551202086916
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-12-06 05:11:49 -0300

    Add mock data for products

---
+++ ShopCart/ShopCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopCartApp.swift
+//  ShopCart
+//
+//  Created by Nikolas Gianoglou Coelho on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
